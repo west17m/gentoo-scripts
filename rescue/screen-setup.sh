2676267c661980cb2d30e34a1c2017ad134bbf1d
Travis Osterman <west17m@users.noreply.github.com>
2019-07-21 14:56:45 -0500

    Update screen-setup.sh

--- rescue/screen-setup.sh
+++ rescue/screen-setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# fix scrolling
+echo 'termcapinfo xterm* ti@:te@' >> ~/.screenrc
+
+#add info line
 echo 'caption always "%{= kw}%-w%{= BW}%n %t%{-}%+w %-= %{y}@%H %{r}%1`%{w}| %{g}%l %{w} | %{y}%m/%d/%Y %c %{w}"' >> ~/.screenrc
 
